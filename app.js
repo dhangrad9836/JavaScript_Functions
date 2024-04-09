6cab750f3483f5555ba53443b8a18c80444e713b
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-04-09 10:35:17 -0400

    completed functions exercise

--- app.js
+++ app.js
@@ -32,6 +32,48 @@ function checkAge(userName = "default username", age = 0) {
   }
 } //end function checkAge
 
+console.log("EXERCISE 3:\n==========\n");
+
+//function to check which quadrant
+function checkQuadrant(x, y) {
+  if (x >= 0 && y >= 0) {
+    console.log("Quadrand 1");
+  } else if (x < 0 && y >= 0) {
+    console.log("Quadrand 2");
+  } else if (x < 0 && y < 0) {
+    console.log("Quadrand 3");
+  } else if (x >= 0 && y < 0) {
+    console.log("Quadrand 4");
+  }
+} //end checkQuadrant
+
+console.log("EXERCISE 4:\n==========\n");
+
+function checkTriagle(len_1, len_2, len_3) {
+  //get sum of len_1 and len_2
+  let sum = len_1 + len_2;
+  let results = "";
+  //check if sum of len_1 and len_2 is less than len_3
+  if (sum < len_3) {
+    return (results = `Invalid triangle!!! the results of ${len_1} and ${len_2} must be greater than ${len_3}`);
+  } else if (len_1 == len_2 && len_2 == len_3) {
+    return (results = `Equilateral triangle`);
+  } else if (sum > len_3 && len_1 == len_2) {
+    return (results = `Isosceles triangle`);
+  } else if (sum > len_3 && len_1 > len_2) {
+    return (results = `Scalene triangle`);
+  }
+} //end checkTriangle
+
+console.log("EXERCISE 5:\n==========\n");
+
+//data plan function
+function mobilePhoneDataUsage(planLimit, day, usage) {
+  //
+} //end mobilePhoneDataUsage
+
 //printOdds(55);
 //checkAge("John", 16);
-checkAge();
+//checkAge();
+//checkQuadrant(0, -18);
+console.log(checkTriagle(3, 1, 45));
